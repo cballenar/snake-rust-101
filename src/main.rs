1f9970fa68b2ed800cfe5def6833d84220cf718a
Carlos Ballena <cballenar@gmail.com>
2022-06-12 14:18:39 -0400

    added snake head and some color to it

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,31 @@ fn setup_camera(mut commands: Commands) {
     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
 }
 
+#[derive(Component)]
+struct SnakeHead;
+
+const SNAKE_HEAD_COLOR: Color = Color::hsla(183.0,0.3,0.7,0.6);
+
+fn spawn_snake(mut commands: Commands) {
+    commands
+        .spawn_bundle(SpriteBundle {
+            sprite: Sprite {
+                color: SNAKE_HEAD_COLOR,
+                ..default()
+            },
+            transform: Transform {
+                scale: Vec3::new(20.0,20.0,20.0),
+                ..default()
+            },
+            ..default()
+        })
+        .insert(SnakeHead);
+}
+
 fn main() {
     App::new()
         .add_startup_system(setup_camera)
+        .add_startup_system(spawn_snake)
         .add_plugins(DefaultPlugins)
         .run();
 }
