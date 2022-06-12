8eb93324ce187473f33be21e2cae0c787623418a
Carlos Ballena <cballenar@gmail.com>
2022-06-12 17:58:58 -0400

    added grid and adapted positioning, scaling, and movement. added background color.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,37 @@
 use bevy::prelude::*;
 
+const ARENA_WIDTH: u32 = 10;
+const ARENA_HEIGHT: u32 = 10;
+const SNAKE_HEAD_COLOR: Color = Color::hsla(183.0,0.3,0.7,0.6);
+const BACKGROUND_COLOR: Color = Color::hsl(183.0,0.3,0.1);
+
 fn setup_camera(mut commands: Commands) {
     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
 }
 
+#[derive(Component, Clone, Copy, PartialEq, Eq)]
+struct Position {
+    x: i32,
+    y: i32,
+}
+
+#[derive(Component)]
+struct Size {
+    width: f32,
+    height: f32,
+}
+
+impl Size {
+    pub fn square(x: f32) -> Self {
+        Self {
+            width: x,
+            height: x,
+        }
+    }
+}
+
 #[derive(Component)]
 struct SnakeHead;
-
-const SNAKE_HEAD_COLOR: Color = Color::hsla(183.0,0.3,0.7,0.6);
 
 fn spawn_snake(mut commands: Commands) {
     commands
@@ -16,40 +40,77 @@ fn spawn_snake(mut commands: Commands) {
                 color: SNAKE_HEAD_COLOR,
                 ..default()
             },
-            transform: Transform {
-                scale: Vec3::new(20.0,20.0,20.0),
-                ..default()
-            },
             ..default()
         })
-        .insert(SnakeHead);
+        .insert(SnakeHead)
+        .insert(Position { x: 3, y: 3 })
+        .insert(Size::square(0.8));
+}
+
+fn size_scaling(windows: Res<Windows>, mut q: Query<(&Size, &mut Transform)>) {
+    let window = windows.get_primary().unwrap();
+    for (sprite_size, mut transform) in q.iter_mut() {
+        transform.scale = Vec3::new(
+            sprite_size.width / ARENA_WIDTH as f32 * window.width() as f32,
+            sprite_size.height / ARENA_HEIGHT as f32 * window.height() as f32,
+            1.0,
+        )
+    }
+}
+
+fn position_translation(windows: Res<Windows>, mut q: Query<(&Position, &mut Transform)>) {
+    fn convert(pos: f32, bound_window: f32, bound_game: f32) -> f32 {
+        let tile_size = bound_window / bound_game;
+        pos / bound_game * bound_window - (bound_window / 2.) + (tile_size / 2.)
+    }
+    let window = windows.get_primary().unwrap();
+    for (pos, mut transform) in q.iter_mut() {
+        transform.translation = Vec3::new(
+            convert(pos.x as f32, window.width() as f32, ARENA_WIDTH as f32),
+            convert(pos.y as f32, window.height() as f32, ARENA_HEIGHT as f32),
+            0.0
+        );
+    }
 }
 
 fn snake_movement(
     keyboard_input: Res<Input<KeyCode>>,
-    mut head_positions: Query<&mut Transform, With<SnakeHead>>
+    mut head_positions: Query<&mut Position, With<SnakeHead>>
 ) {
-    for mut transform in head_positions.iter_mut() {
+    for mut pos in head_positions.iter_mut() {
         if keyboard_input.pressed(KeyCode::Up) {
-            transform.translation.y += 2.;
+            pos.y += 1;
         }
         if keyboard_input.pressed(KeyCode::Right) {
-            transform.translation.x += 2.;
+            pos.x += 1;
         }
         if keyboard_input.pressed(KeyCode::Down) {
-            transform.translation.y -= 2.;
+            pos.y -= 1;
         }
         if keyboard_input.pressed(KeyCode::Left) {
-            transform.translation.x -= 2.;
+            pos.x -= 1;
         }
     }
 }
 
 fn main() {
     App::new()
+        .insert_resource(WindowDescriptor {
+            title: "Snake!".to_string(),
+            width: 500.0,
+            height: 500.0,
+            ..default()
+        })
+        .insert_resource(ClearColor(BACKGROUND_COLOR))
         .add_startup_system(setup_camera)
         .add_startup_system(spawn_snake)
         .add_system(snake_movement)
+        .add_system_set_to_stage(
+            CoreStage::PostUpdate,
+            SystemSet::new()
+                .with_system(position_translation)
+                .with_system(size_scaling),
+        )
         .add_plugins(DefaultPlugins)
         .run();
 }
